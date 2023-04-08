0572e19e0fc9a2efef9061486d648c13791808e5
Shashank29078 <shashanknalogal@gmail.com>
2023-04-08 17:50:41 +0530

    modal cart commit

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -6,7 +6,7 @@ const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
-        <h1>Chandrashekar N M Meals</h1>
+        <h1>React Meals</h1>
         <HeaderCartButton />
       </header>
       <div className={classes["main-image"]}>
